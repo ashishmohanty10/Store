1542243658cb115a194beca2661a2e5fba2ff156
ASHISH MOHANTY <ashishmohaty10@gmail.com>
2024-06-21 23:49:14 +0530

    admin product page done

--- src/app/admin/_action/products.ts
+++ src/app/admin/_action/products.ts
@@ -60,6 +60,63 @@ export async function addProduct(prevState: unknown, formData: FormData) {
   redirect("/admin/products");
 }
 
+const editSchema = addSchema.extend({
+  file: fileSchema.optional(),
+  image: imageSchema.optional(),
+});
+
+export async function updateProduct(
+  id: string,
+  prevState: unknown,
+  formData: FormData
+) {
+  const result = editSchema.safeParse(Object.fromEntries(formData.entries()));
+  if (result.success === false) {
+    return result.error.formErrors.fieldErrors;
+  }
+
+  const data = result.data;
+  const product = await prisma.product.findUnique({
+    where: { id },
+  });
+
+  if (product == null) return notFound();
+
+  let filepath = product.filepath;
+
+  if (data.file != null && data.file.size > 0) {
+    await fs.unlink(product.filepath);
+    //create the filepath for the  file to be saved
+    filepath = `product/${crypto.randomUUID()}-${data.file.name}`;
+    //saves the file to the filepath
+    await fs.writeFile(filepath, Buffer.from(await data.file.arrayBuffer()));
+  }
+
+  let imagePath = product.imagePath;
+
+  if (data.image != null && data.image.size > 0) {
+    await fs.unlink(`public/${product.imagePath}`);
+    const imagePath = `/product/${crypto.randomUUID()}-${data.image.name}`;
+    //saves the images to the filepath
+    await fs.writeFile(
+      `public${imagePath}`,
+      Buffer.from(await data.image.arrayBuffer())
+    );
+  }
+
+  await prisma.product.update({
+    where: { id },
+    data: {
+      name: data.name,
+      description: data.description,
+      priceInRupees: data.priceInRupees,
+      filepath,
+      imagePath,
+    },
+  });
+
+  redirect("/admin/products");
+}
 export async function toggleProductAvailability(
   id: string,
   isAvailableForPurchase: boolean
--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,23 +1,37 @@
 "use client";
 
-import { addProduct } from "@/app/admin/_action/products";
+import { addProduct, updateProduct } from "@/app/admin/_action/products";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { formatCurrency } from "@/lib/formatter";
+import { Product } from "@prisma/client";
+import Image from "next/image";
 import { useState } from "react";
 import { useFormState, useFormStatus } from "react-dom";
 
-export function ProductForm() {
-  const [error, actions] = useFormState(addProduct, {});
+export function ProductForm({ product }: { product?: Product | null }) {
+  const [error, actions] = useFormState(
+    product == null ? addProduct : updateProduct.bind(null, product.id),
+    {}
+  );
 
-  const [priceInRupees, setPriceInRupees] = useState<number>();
+  const [priceInRupees, setPriceInRupees] = useState<number | undefined>(
+    product?.priceInRupees
+  );
   return (
     <form action={actions} className="space-y-8">
       {/* name */}
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required />
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          required
+          defaultValue={product?.name || ""}
+        />
         {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
 
@@ -45,7 +59,12 @@ export function ProductForm() {
       {/* Descriptiom */}
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
-        <Input type="text" id="description" name="description" required />
+        <Textarea
+          id="description"
+          name="description"
+          required
+          defaultValue={product?.description || ""}
+        />
         {error.description && (
           <div className="text-destuctive">{error.description}</div>
         )}
@@ -54,14 +73,26 @@ export function ProductForm() {
       {/* File */}
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
-        <Input type="file" id="file" name="file" required />
+        <Input type="file" id="file" name="file" required={product == null} />
+
+        {product != null && (
+          <div className="text-muted-foreground">{product?.filepath}</div>
+        )}
         {error.file && <div className="text-destructive">{error.file}</div>}
       </div>
 
       {/* Image */}
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
-        <Input type="file" id="image" name="image" required />
+        <Input type="file" id="image" name="image" required={product == null} />
+        {product != null && (
+          <Image
+            src={product?.imagePath}
+            height={400}
+            width={400}
+            alt="product Image"
+          />
+        )}
         {error.image && <div className="text-destuctive">{error.image}</div>}
       </div>
 
